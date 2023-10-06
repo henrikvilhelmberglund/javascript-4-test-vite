6983f143aefe1acc827cb7baeb8ddce51cdbdd93
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-06 12:48:34 +0200

    fix: :bug: add spaces for color values

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function Movies() {
-  const [backgroundColor, setBackgroundColor] = useState("rgb(0,255,0)");
+  const [backgroundColor, setBackgroundColor] = useState("rgb(0, 255, 0)");
   const [checked, setChecked] = useState(false);
   return (
     <>
@@ -11,13 +11,13 @@ export default function Movies() {
         id="my-button"
         onClick={() => {
           const newBackgroundColor =
-            backgroundColor === "rgb(0,255,0)"
-              ? "rgb(255,0,0)"
-              : "rgb(0,255,0)";
+            backgroundColor === "rgb(0, 255, 0)"
+              ? "rgb(255, 0, 0)"
+              : "rgb(0, 255, 0)";
           setBackgroundColor(newBackgroundColor);
         }}
         style={{ backgroundColor: backgroundColor }}>
-        {backgroundColor === "rgb(0,255,0)"
+        {backgroundColor === "rgb(0, 255, 0)"
           ? "Gör till favorit"
           : "Ta bort favorit"}
       </button>
